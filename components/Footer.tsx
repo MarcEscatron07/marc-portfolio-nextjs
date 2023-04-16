af536f73042ffe7ae2314cf9b4dfc054e4042901
Marc Benedict Escatron <marc.escatron07@gmail.com>
2023-04-16 11:29:42 +0800

    created initial components

---
+++ components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <>
+        <footer className="navbar-footer">
+            <p className="m-0">&copy; Marc Escatron 2023. All Rights Reserved.</p>
+        </footer>
+    </>    
+  )
+}
+
+export default Footer
