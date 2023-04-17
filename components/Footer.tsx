5ed1faefcb0ddf8ecb2360ade69ded06b0c1cb6e
Marc Benedict Escatron <marc.escatron07@gmail.com>
2023-04-17 19:48:15 +0800

    added color themes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ function Footer() {
     <>
         <footer className="navbar-footer">
             <p className="m-0">&copy; 2023 Marc Benedict Escatron. 
-            Built with <span><Link href="https://nextjs.org/" target="_blank">NEXT.JS</Link></span>.</p>
+            Built with <span><Link className="themed-link" href="https://nextjs.org/" target="_blank">NEXT.JS</Link></span>.</p>
         </footer>
     </>    
   )
