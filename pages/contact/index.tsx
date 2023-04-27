bbb721ace9d455966e068ba27ba77a72e046a726
Marc Benedict Escatron <marc.escatron07@gmail.com>
2023-04-28 06:16:35 +0800

    added handling of spamming Send button in Contact section

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -38,6 +38,10 @@ function Contact() {
     e.preventDefault();
     // console.log('Contact > onFormSubmit event', e)
 
+    if(isLoading) {
+      return;
+    }
+
     const payload: any = {
       ...contactData,
       subject: 'Marc Escatron - Portfolio (Client)'
@@ -127,8 +131,8 @@ function Contact() {
                   <Button 
                     type="submit"
                     variant="outlined"
-                    endIcon={isLoading ? <CircularProgress color="secondary" size={20} /> : null}
                     color="secondary"
+                    endIcon={isLoading ? <CircularProgress color="secondary" size={20} /> : null}
                   >
                     Send
                   </Button>
