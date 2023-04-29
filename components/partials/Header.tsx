f82098803b1cdd137b1a99d2f58b9d75a6f19836
Marc Benedict Escatron <marc.escatron07@gmail.com>
2023-04-29 11:09:04 +0800

    changed color scheme of logo

--- components/partials/Header.tsx
+++ components/partials/Header.tsx
@@ -27,7 +27,7 @@ function Header() {
         <div className="container">
           <Link className="navbar-brand" href="/">
             <Image
-              src="/images/logo-mbe.png"
+              src="/images/logo-image.png"
               alt="navbar-logo"
               height={70}
               width={70}
